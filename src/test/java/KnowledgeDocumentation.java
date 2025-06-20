ca142b63aa98062908ada8d0d69e6334958cff3b
Rintu <rintu_8@rediffmail.com>
2025-06-20 14:39:02 +0530

    Updated knowledge documentation file

--- src/test/java/KnowledgeDocumentation.java
+++ src/test/java/KnowledgeDocumentation.java
@@ -1,5 +1,51 @@
 
 public class KnowledgeDocumentation {
+	
+	
+	
+	
+	
+	//Important youtube links
+//	1. https://www.youtube.com/watch?v=rb5OL-QQPw4&t=623s : Selenium + Jenkins + GIT Integration : Run your Test 
+	//Cases from GIT Hub using Jenkins
+	
+	// 2. https://www.youtube.com/watch?v=LxJzeiTQGoE : Page Object Model (POM) 
+	//Design With Selenium - Part -2
+
+
+//To explain POM, i have created a package "Pages" and "test" under src/test/java.In "Pages, 
+	//i have created a class "google search page".In test , 
+	//i have created "googlesearch.java".This combined is the structure of POM
+//You can observe that in Google search.java, i am importing "googlesearchpage.java" , 
+//because i hve created page objects on googlesearchpage.java, and i can call these
+//page objects from googlesearch.java
+ 
+//What is Page object model : its a design pattern to manage the testcases and the page objects
+	
+//============================================
+
+//This is link for cucumber : https://www.youtube.com/watch?v=4e9vhX7ZuCw
+/* for cucumner i have created 2 classes.First i created  a folder Features 
+ * under src/test/resources. Under features, i created a file "Login1.feature".
+ * In Login 1, i have writtem the cucumber testcase.
+ * Under  src/test/java , i have created a folder "StepDefs".Under this folder, i have created
+ * class "LoginSteps.java"
+ * For each of the step in "Login1.feature" , i have created a method in "LoginSteps.java"
+ * Some of the preconditions for cucumber is adding dependencies for cucumber.
+ * Also on eclipse, go to Help>>eclipse marketplace and install cucumber dependency
+ * On the "Login1.feature" , right click and run as cucumber
+ * 
+ * 
+ * ======================================================== */
+	
+	//Jenkins : Can refer video : https://www.youtube.com/watch?v=lPxrPqSyCo8 and 
+	//https://www.youtube.com/watch?v=rb5OL-QQPw4&t=623s
+	
+	//commant to launch jenkins
+	//java -jar C:\Users\rintu.george\Downloads\jenkins.war --enable-future-java
+
+	
+	//===========================================================================
 
 	
 	/* 
